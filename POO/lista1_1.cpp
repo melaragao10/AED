79e21d79b823d5a9f4eb65e038ff617699325648
melaragao10 <124120159+melaragao10@users.noreply.github.com>
2024-03-21 11:09:02 -0300

    Update lista1_1.cpp

--- POO/lista1_1.cpp
+++ POO/lista1_1.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class AgendaTelefonica 
+{
+private:
+    string nome;
+    string telefone;
+
+public:
+
+    void setNome(const string &n) 
+    {
+        nome = n;
+    }
+
+    void setTelefone(const string &t) {
+        telefone = t;
+    }
+
+    void imprimirDados() const {
+        cout << "Nome: " << nome << ", Telefone: " << telefone << "\n";
+    }
+};
+
+int main() {
+
+    AgendaTelefonica agenda;
+    agenda.setNome("Melissa Aragão");
+    agenda.setTelefone("1234-5678");
+
+    agenda.imprimirDados();
+
+    return 0;
+}
 
